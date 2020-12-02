2f308c8b76c52d0b6e135ecf22b3a6ac603a0099
liakassardjian <liakassardjian@gmail.com>
2020-12-02 17:59:26 -0300

    Personal repository

---
+++ javascript/data.js
@@ -0,0 +1,102 @@
+const apps = [
+    {
+        name: "Purple Notebook",
+        description: "The Purple Notebook is an app built from students to students. An app thought to be the best notes app for students, created in a way to make note taking easier and faster during class. Using Apple's Vision framework, students can even create notes from pictures they've taken.",
+        roles: "Code QA and Developer",
+        development: "UIKit, View Code, MacCatalyst, Vision, CoreData, CloudKit",
+        appStoreLink: "https://apps.apple.com/us/app/purple-notebook/id1535269503",
+        gitHubLink: "https://github.com/Pepelelipa/MacroChallenge",
+        imagePath: "./assets/purplenotebook.png",
+        platforms: "iOS, iPadOS and macOS"
+    },
+    {
+        name: "Generate",
+        description: "Generate is a screen saver application for Apple TV that exhibit generative artwork. Thinking about encouraging people to know more about science. This app shows more information about who created the fractals and a little of their history. The user of the app can pause, take a print and can save different patterns of the art.",
+        roles: "Developer",
+        development: "Metal, TVOS, UIKit, Storyboard and MVC",
+        appStoreLink: "https://apps.apple.com/br/app/generate/id1524091697",
+        gitHubLink: "https://github.com/guienes/ArteGenerativa-AppleTV",
+        imagePath: "./assets/generate.png",
+        platforms: "tvOS"
+    },
+    {
+        name: "Proficiente",
+        description: "Proficiente is an app built to bring transparency to accessibility in the market. In this app, you can check information on companies and leave reviews about yout experience as an employee, so that people can know if a company is accessible or not.",
+        roles: "SCRUM Master and developer",
+        development: "UIKit, Storyboard, VoiceOver, MVC and APIs",
+        appStoreLink: "https://apps.apple.com/br/app/proficiente/id1480049565",
+        gitHubLink: "https://github.com/LuizHMC/acessibilidade_mc3",
+        imagePath: "./assets/proficiente.png",
+        platforms: "iOS"
+    },
+    {
+        name: "Gravity",
+        description: "Gravity is a personal project and a purely entertaining app, with colorful little balls that roll from one side to another, according to gravity's direction. This app was entirely built with UIKit, with the purpose of studying the framework's animation possibilities.",
+        roles: "Product owner and Developer",
+        development: "UIKit",
+        appStoreLink: "",
+        gitHubLink: "https://github.com/liakassardjian/Gravity",
+        imagePath: "./assets/gravity.png",
+        platforms: "iOS"
+    },
+    {
+        name: "Festou!",
+        description: "Festou! is an app that helps you to plan your parties based on the number of guests and the party's duration. The app gives you a checklist so you can make sure to get everything you need for your party.",
+        roles: "Developer",
+        development: "UIKit, Storyboard, MVC and CoreData",
+        appStoreLink: "https://apps.apple.com/br/app/festou/id1463869085?l=en",
+        gitHubLink: "",
+        imagePath: "./assets/festou.png",
+        platforms: "iOS"
+    },
+    {
+        name: "Hora do almoço",
+        description: "With this app for the Apple Watch, you can receive daily sugestions of new places to eat based on your preferences and proximity. This is a new way to choose where to go at lunch time!",
+        roles: "Developer",
+        development: "WatchOS, UIKit and APIs",
+        appStoreLink: "https://apps.apple.com/br/app/hora-do-almo%C3%A7o/id1503252410",
+        gitHubLink: "",
+        imagePath: "./assets/horadoalmoco.png",
+        platforms: "WatchOS"
+    },
+    {
+        name: "Diário de oração",
+        description: "This personal project is an app to help people keep track of their meditation moments, allowing them to follow a prayer list and register their Bible reading over time.",
+        roles: "Product owner and developer",
+        development: "UIKit, Storyboard, MVC and CoreData",
+        appStoreLink: "https://apps.apple.com/br/app/diário-de-oração/id1474562896",
+        gitHubLink: "",
+        imagePath: "./assets/diario.png",
+        platforms: "iOS"
+    },
+    {
+        name: "Kaligo",
+        description: "Kaligo is an app created to help people who want to continue their studies. In this app, you can create study script, which contain steps to guide you in your studying process.",
+        roles: "Developer",
+        development: "UIKit, Storyboard, MVC and APIs",
+        appStoreLink: "https://apps.apple.com/us/app/kaligo/id1515977370",
+        gitHubLink: "https://github.com/Kal1go/Kaligo",
+        imagePath: "./assets/kaligo.png",
+        platforms: "iOS"
+    },
+    {
+        name: "Novo Oeste",
+        description: "This is a decision game in which your choices have an effect on the story, with a very fun theme and characters that make you rethink you decision. Help the citizens of Novo Oeste to choose their new sheriff!",
+        roles: "Sound designer",
+        development: "Sprite Kit and AVFoundation",
+        appStoreLink: "https://apps.apple.com/us/app/novo-oeste/id1492530457m",
+        gitHubLink: "https://github.com/faguilhermo/wildWestGame",
+        imagePath: "./assets/novooeste.png",
+        platforms: "iOS"
+    },
+    {
+        name: "React",
+        description: "React is an application that uses augmented reality with iamge tracking to be able to teach chemistry in an easier and more intuitive way for students. The idea of a solution that can be implemented by schools!",
+        roles: "Designer",
+        development: "ARKit and SceneKit",
+        appStoreLink: "",
+        gitHubLink: "https://github.com/andradejoaoh/ARKit/tree/developer",
+        imagePath: "./assets/react.png",
+        platforms: "iOS"
+    }
+];
---
+++ javascript/displayPortfolio.js
@@ -0,0 +1,104 @@
+function displayPortfolio() {
+    const section = document.getElementById("portfolio");
+    const row = createCustomElement("div", "row"); 
+
+    for (var i = 0; i < apps.length; i++) {
+        row.appendChild(createCard(apps[i]));
+    }
+
+    section.appendChild(row);
+}
+
+function createCard(app) {
+    const column = createCustomElement("div", "card-column col-xl-4 col-lg-4 col-md-6 col-sm-6 d-flex align-items-stretch");
+    const card = createCustomElement("div", "card border-0");
+    
+    const image = createCustomElement("img", "card-img-top");
+    image.src = app.imagePath;
+    image.alt = app.name + " icon";
+    card.appendChild(image);
+
+    card.appendChild(createCardBody(app));
+    card.appendChild(createCardFooter(app));
+    column.appendChild(card);
+
+    return column;
+}
+
+function createCardFooter(app) {
+    const cardFooter = createCustomElement("div", "card-footer border-0");
+    const hasAppStoreLink = (app.appStoreLink != "");
+    const hasGitHubLink = (app.gitHubLink != "");
+
+    if (hasAppStoreLink) {
+        cardFooter.appendChild(createLink(app.appStoreLink, "App Store"));   
+    }
+
+    if (hasGitHubLink) {
+        cardFooter.appendChild(createLink(app.gitHubLink, "GitHub", hasAppStoreLink));
+    }
+
+    return cardFooter;
+
+}
+
+function createCardBody(app) {
+    const cardBody = createCustomElement("div", "card-body");
+
+    const appName = createCustomElement("h3", "card-title");
+    appName.innerHTML = app.name;
+    cardBody.appendChild(appName);
+
+    cardBody.appendChild(createOSLabel(app.platforms));
+
+    const appDescription = createCustomElement("p", "card-text");
+    appDescription.innerHTML = app.description;
+    cardBody.appendChild(appDescription);
+
+    cardBody.appendChild(createTextGroup("Roles", app.roles));
+    cardBody.appendChild(createTextGroup("Development", app.development));
+
+    return cardBody;
+}
+
+function createTextGroup(title, body) {
+    const group = createCustomElement("div", "text-group");
+
+    const titleElement = createCustomElement("h6", "font-weight-bold");
+    titleElement.innerHTML = title;
+
+    const bodyElement = createCustomElement("p", "");
+    bodyElement.innerHTML = body;
+
+    group.appendChild(titleElement);
+    group.appendChild(bodyElement);
+    return group;
+}
+
+function createLink(url, label, spacing = false) {
+    var className = "badge badge-pill badge-light";
+    if (spacing) {
+        className += " ml-2";
+    }
+
+    const link = createCustomElement("a", className);
+    link.href = url;
+    link.innerHTML = label;
+    return link;
+}
+
+function createOSLabel(os) {
+    const p = document.createElement("p");
+    const small = document.createElement("small");
+
+    small.innerHTML = os;
+    p.appendChild(small);
+
+    return p;
+}
+
+function createCustomElement(elementName, className) {
+    const element = document.createElement(elementName);
+    element.className = className;
+    return element
+}
